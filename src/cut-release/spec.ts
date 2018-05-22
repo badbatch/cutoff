56bdff317a633491d82a4c25c3906ea12c04278b
Dylan Aubrey <dylanaubrey@gmail.com>
2018-05-22 08:52:59 +0100

    Removing stale spec file.

--- src/cut-release/spec.ts
+++
@@ -1,8 +0,0 @@
-import { cutRelease } from "~/cut-release";
-import { addCommitPush } from "~/helpers/add-commit-push";
-import { checkoutMaster } from "~/helpers/checkout-master";
-import { getNewVersion } from "~/helpers/get-new-version";
-
-test("", () => {
-
-});
