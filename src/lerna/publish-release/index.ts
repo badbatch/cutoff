d60f9d0b5abd4df94da2f14461c2ad7309e4fdac
Haaris M <haaris.m.india@gmail.com>
2019-06-28 19:37:06 +0530

    enhance(publish-lerna-cutoff): option to control concurrency (#13)

--- src/lerna/publish-release/index.ts
+++ src/lerna/publish-release/index.ts
@@ -1,10 +1,12 @@
 import { resolve } from "path";
 import shell from "shelljs";
+import yargs from "yargs";
 import { UpdatedPackage } from "../../types";
 
 export default function publishLernaRelease(): void {
   const updatedConfigPath = resolve(process.cwd(), ".lerna.updated.json");
   const updated: UpdatedPackage[] = require(updatedConfigPath) || [];
   const names = updated.map((pkg) => pkg.name);
-  shell.exec(`lerna exec --parallel -- publish-lerna-cutoff-pkg --packages ${names.join(" ")}`);
+  const concurrency: number = yargs.number("concurrency").parse().concurrency || 4;
+  shell.exec(`lerna exec --concurrency ${concurrency} -- publish-lerna-cutoff-pkg --packages ${names.join(" ")}`);
 }
